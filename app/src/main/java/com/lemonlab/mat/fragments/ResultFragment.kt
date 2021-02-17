a573e7385aeb955694b939b32ccbfae924cedbb8
IbrahimAlatoom <ibrahim.alatoom@gmail.com>
2021-02-17 21:03:28 +0200

    navigate

--- app/src/main/java/com/lemonlab/mat/fragments/ResultFragment.kt
+++ app/src/main/java/com/lemonlab/mat/fragments/ResultFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.lemonlab.mat.R
 import com.lemonlab.mat.databinding.FragmentResultBinding
 
 
 class ResultFragment : Fragment() {
     private lateinit var binding : FragmentResultBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,13 +27,17 @@ class ResultFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_result,container,false)
 
         val args = ResultFragmentArgs.fromBundle(requireArguments())
-        when (args.yesesCounter) {
+
+        when (args.yesesCounter){
             in 0..2 -> binding.resultText.text = "سلوكك طبيعي ولكن هذا لا يعني أنك يجب أن تعيش على هاتفك الذكي"
             in 3..4 -> binding.resultText.text = "سلوكك يميل نحو الاستخدام القهري(الإدماني)."
             in 5..7 -> binding.resultText.text = "من المحتمل أن يكون لديك نمط استخدام هاتف ذكي قهري."
             in 8..15 -> binding.resultText.text = "تفكر في زيارة طبيب نفسي أو معالج نفسي متخصص في الإدمان السلوكي للحصول على استشارة."
         }
-
+        binding.backButton.setOnClickListener(){
+            navController = findNavController()
+            navController.navigate(R.id.action_resultFragment_to_titleFragment)
+        }
         return binding.root
 
     }
