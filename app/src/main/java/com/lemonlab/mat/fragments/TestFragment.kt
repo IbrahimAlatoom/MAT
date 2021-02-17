85c466b86eb17a7b6a7217ff92ce319717d778e2
IbrahimAlatoom <ibrahim.alatoom@gmail.com>
2021-02-17 14:27:18 +0200

    Initial commit

---
+++ app/src/main/java/com/lemonlab/mat/fragments/TestFragment.kt
@@ -0,0 +1,33 @@
+package com.lemonlab.mat.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.lemonlab.mat.R
+import com.lemonlab.mat.databinding.FragmentTestBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class TestFragment : Fragment() {
+    private lateinit var binding : FragmentTestBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_test,container,false)
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/lemonlab/mat/fragments/TitleFragment.kt
@@ -0,0 +1,39 @@
+package com.lemonlab.mat.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.lemonlab.mat.R
+import com.lemonlab.mat.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+    private lateinit var binding: FragmentTitleBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        return binding.root
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener(
+            Navigation.createNavigateOnClickListener(
+                R.id.action_titleFragment_to_testFragment
+            )
+        )
+    }
+}
