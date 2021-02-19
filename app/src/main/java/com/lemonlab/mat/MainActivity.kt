878f2250e7990c3dd64887783ae4816374edb2a0
IbrahimAlatoom <ibrahim.alatoom@gmail.com>
2021-02-19 21:02:34 +0200

    admob added

--- app/src/main/java/com/lemonlab/mat/MainActivity.kt
+++ app/src/main/java/com/lemonlab/mat/MainActivity.kt
@@ -3,6 +3,8 @@ package com.lemonlab.mat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import com.lemonlab.mat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        MobileAds.initialize(this) {}
+
+        val mAdView = binding.adView
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
